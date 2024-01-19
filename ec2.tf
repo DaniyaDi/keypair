bd0364fada229d9cca107875416d8098af2a44ec
DaniyaDi <145853610+DaniyaDi@users.noreply.github.com>
2024-01-18 19:29:23 -0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "task" {
-    ami =  "ami-0cd3c7f72edd5b06d"
-    instance_type = "t2.micro"
-    key_name      = aws_key_pair.dana
+  ami           = "ami-0cd3c7f72edd5b06d"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.dana
 
-    tags ={
-        Name = "task instance"
-    }
+  tags = {
+    Name = "task instance"
+  }
 }
