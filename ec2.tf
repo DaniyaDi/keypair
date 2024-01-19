05b68801a28bfe87b2f6e684f73acdfbb8af2f79
DaniyaDi <145853610+DaniyaDi@users.noreply.github.com>
2024-01-18 19:28:18 -0600

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "task" {
+    ami =  "ami-0cd3c7f72edd5b06d"
+    instance_type = "t2.micro"
+    key_name      = aws_key_pair.dana
+
+    tags ={
+        Name = "task instance"
+    }
+}
