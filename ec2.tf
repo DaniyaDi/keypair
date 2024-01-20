b0b0169831c51f2e6c81d06f135ffaa3e809b5a6
DaniyaDi <145853610+DaniyaDi@users.noreply.github.com>
2024-01-20 09:16:38 -0600

    Delete ec2.tf

--- ec2.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "task" {
-  ami           = "ami-0cd3c7f72edd5b06d"
-  instance_type = "t2.micro"
-  key_name      =  "dana"
-
-  tags = {
-    Name = "task-instance"
-  }
-}
