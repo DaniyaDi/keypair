266c001f0c7aea304e38464340bcafce70944688
DaniyaDi <145853610+DaniyaDi@users.noreply.github.com>
2024-01-20 09:32:14 -0600

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,3 @@
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-  tags = {
-    Name = "task-vpc"
-  }
+module "aws-vpc-module" {
+  source = "github.com/DaniyaDi/terraform-aws-vpc//modules/aws-vpc-module"    #source = "./modules/aws-vpc-module"
 }
